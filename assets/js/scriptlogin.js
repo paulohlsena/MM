e59ae0a8cc387bdcc7745851548ee79bedb37bff
Paulo Sena <147195085+paulohlsena@users.noreply.github.com>
2024-01-07 23:18:34 -0300

    Delete assets directory

--- assets/js/scriptlogin.js
+++
@@ -1,9 +0,0 @@
-function acesso(){
-    varUsu = document.getElementById("usu").value;
-    varSenha = document.getElementById("senha").value;
-    if (varUsu == "agendamento" && varSenha == "123") {
-    alert("Acesso permitido")
-    location.href = "page1.html"
-    } else{
-     alert ("Acesso negado") }
-    }
